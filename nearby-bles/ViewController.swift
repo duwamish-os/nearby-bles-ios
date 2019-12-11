049f52b94da176bd369d9dcab1a231738f471feb
Upadhyay <prayag.upd@gmail.com>
2019-12-11 11:48:37 -0800

    2019/12/11 - add hello world for ios app in swift

---
+++ nearby-bles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nearby-bles
+//
+//  Created by Upadhyay, Prayag on 12/11/19.
+//  Copyright © 2019 nearby-bles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nearby-blesTests/nearby_blesTests.swift
@@ -0,0 +1,34 @@
+//
+//  nearby_blesTests.swift
+//  nearby-blesTests
+//
+//  Created by Upadhyay, Prayag on 12/11/19.
+//  Copyright © 2019 nearby-bles. All rights reserved.
+//
+
+import XCTest
+@testable import nearby_bles
+
+class nearby_blesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
